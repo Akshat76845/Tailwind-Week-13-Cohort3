e029158165906b3d7036d3e2ab57e709be630088
Akshat <akshatgupta78886@gmail.com>
2025-02-03 10:33:09 +0530

    Grid in Tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ export default function App(){
   return (
 
   <>
- <div className="sm:bg-red-300 bg-green-500">hello
-    </div>
+ <div className="grid grid-cols-12">
+  <div className="bg-blue-300 col-span-4">hi</div>
+  <div className="bg-green-400 col-span-6">hello</div>
+  <div className="bg-red-300 col-span-2">hey</div>
+ </div>
   </>
   );
 }
